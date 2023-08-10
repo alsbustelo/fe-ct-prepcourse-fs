578895e8b01a8b5800133eefd4a44b597730fba1
alsbustelo <alsbustelo@gmail.com>
2023-08-09 22:31:00 -0300

    t

---
+++ CPH/lan.js
@@ -0,0 +1,19 @@
+/*Tipos de datos primitivos:
+
+Number: Almacena números, ya sean enteros o de punto flotante.
+String: Almacena cadenas de texto, como palabras o frases.
+Boolean: Representa un valor verdadero (true) o falso (false).
+Null: Representa la ausencia intencional de un valor.
+Undefined: Representa una variable que ha sido declarada pero aún no tiene un valor asignado.
+Symbol: Introducido en ECMAScript 6, se utiliza para crear identificadores únicos, principalmente para propiedades de objetos.
+BigInt: Introducido en ECMAScript 11, permite representar números enteros mayores de 2^53 - 1.
+Tipos de datos compuestos (objetos):
+
+Object: Una estructura de datos que almacena pares de clave y valor, donde la clave es una cadena (o Symbol) y el valor puede ser de cualquier tipo de dato, incluyendo otros objetos.
+Array: Una estructura ordenada que almacena una lista de valores, generalmente del mismo tipo, y se accede a ellos mediante índices numéricos.
+Function: Un objeto que puede contener un bloque de código ejecutable y puede ser llamado.
+Date: Se utiliza para trabajar con fechas y horas.
+RegExp: Representa una expresión regular, que se utiliza para buscar y manipular cadenas de texto basadas en patrones.
+Map: Una colección de pares clave-valor que permite utilizar cualquier tipo de dato como clave.
+Set: Una colección de valores únicos, lo que significa que no puede haber duplicados en el conjunto.
+... (otros objetos internos y personalizados): Además de los tipos mencionados, puedes crear tus propios objetos personalizados y JavaScript también tiene tipos de datos internos como Error, Promise, ArrayBuffer, entre otros. */
